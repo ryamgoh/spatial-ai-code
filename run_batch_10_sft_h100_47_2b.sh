544c1d2317dea8137094659d106b5e6ebe3cc555
Ryann Goh <ryanngohenrui@gmail.com>
2026-09-07 22:02:10 +0800

    UPdate ram

--- run_batch_10_sft_h100_47_2b.sh
+++ run_batch_10_sft_h100_47_2b.sh
@@ -10,7 +10,7 @@
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=16
-#SBATCH --mem=256G
+#SBATCH --mem=128G
 #SBATCH --time=3-00:00:00
 #SBATCH --gres=gpu:h100-47:1
 #SBATCH --output=logs/%x-%j.out
